2c1ec0cad676cef6292cebfe4b847bec7c812fe6
yakovalenko <jaroslawkovalenko@jmail.com>
2019-06-19 11:43:34 +0300

    add run class

---
+++ src/main/java/com/project/calculator/CalculatorApplication.java
@@ -0,0 +1,16 @@
+package com.project.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+
+public class CalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
+
+}
