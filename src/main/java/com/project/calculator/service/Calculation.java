4d61f34e029245f56338bbab63f8ea92f9c77ed2
yakovalenko <jaroslawkovalenko@jmail.com>
2019-06-19 11:54:36 +0300

    adding calculation methods to service

---
+++ src/main/java/com/project/calculator/service/Calculation.java
@@ -0,0 +1,34 @@
+package com.project.calculator.service;
+
+import com.service.calculator.model.CalculationModel;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+@Service
+public class Calculation {
+
+    public int sum(CalculationModel calculationModel) {
+
+        return calculationModel.getA() + calculationModel.getB();
+
+    }
+
+    public int sub(CalculationModel calculationModel) {
+
+        return calculationModel.getA() - calculationModel.getB();
+
+    }
+
+    public int mult(CalculationModel calculationModel) {
+
+        return calculationModel.getA() * calculationModel.getB();
+
+    }
+
+    public int div(CalculationModel calculationModel) {
+
+        return calculationModel.getA() / calculationModel.getB();
+
+    }
+
+}
